8cc8c518571b6f5c593a910068ef5011dbb91df4
Christux <christux57@free.fr>
2025-07-10 21:15:19 +0200

    add doctring

--- src/chx_matrix.pyi
+++ src/chx_matrix.pyi
@@ -7,51 +7,50 @@ class Matrix:
     """
 
     def __init__(self, rows: int, cols: int) -> None:
-        """_summary_
+        """Initializes the matrix
 
         Args:
-            rows (int): _description_
-            cols (int): _description_
+            rows (int): number of rows
+            cols (int): number of columns
         """
 
     @property
     def shape(self) -> tuple[int, int]:
-        """_summary_
+        """Gets the shape of the matrix
 
-        Args:
-            self (_type_): _description_
-            int (_type_): _description_
+        Returns:
+            tuple[int, int]: the shape of the matrix
         """
 
     def get(self, i: int, j: int) -> float:
-        """_summary_
+        """Gets a value of the matrix
 
         Args:
-            i (int): _description_
-            j (int): _description_
+            i (int): row index
+            j (int): column index
 
         Returns:
-            float: _description_
+            float: value
         """
 
     def set(self, i: int, j: int, value: float) -> None:
-        """_summary_
+        """Sets a value of the matrix
 
         Args:
-            i (int): _description_
-            j (int): _description_
-            value (float): _description_
+            i (int): row index
+            j (int): column index
+            value (float): value
         """
 
     def __getitem__(self, index):
-        """_summary_
+        """Gets value
 
         Args:
             index (_type_): _description_
         """
 
     def __setitem__(self, index, value) -> None:
-        """_summary_
+        """Sets value
 
         Args:
             index (_type_): _description_
@@ -59,107 +58,110 @@ class Matrix:
         """
 
     def set_all(self, value: float) -> 'Matrix':
-        """_summary_
+        """Sets all values of the matrix
 
         Args:
-            value (float): _description_
+            value (float): value
 
         Returns:
-            Matrix: _description_
+            Matrix: the matrix itself
         """
 
     def transpose(self) -> 'Matrix':
-        """_summary_
+        """Transposes the matrix
 
         Returns:
-            Matrix: _description_
+            Matrix: the transposed matrix
         """
 
     def copy(self) -> 'Matrix':
-        """_summary_
+        """Copies the matrix
 
         Returns:
-            Matrix: _description_
+            Matrix: the duplicated matrix
         """
 
     def get_sub_matrix(self, row_start: int, row_end: int, col_start: int, col_end: int) -> 'Matrix':
-        """_summary_
+        """Gets a part of the matrix
 
         Args:
-            row_start (_type_): _description_
-            row_end (_type_): _description_
-            col_start (_type_): _description_
-            col_end (_type_): _description_
+            row_start (_type_): start row index
+            row_end (_type_): end row index
+            col_start (_type_): start column index
+            col_end (_type_): end column index
 
         Returns:
-            Matrix: _description_
+            Matrix: the part of the matrix
         """
 
-    def get_row(self, row_idx:int) -> 'Row':
-        """_summary_
+    def get_row(self, row_idx: int) -> 'Row':
+        """Gets a row of the matrix
 
         Args:
-            row_idx (int): _description_
+            row_idx (int): row index
 
         Returns:
-            Row: _description_
+            Row: the row
         """
 
     def get_column(self, col_idx: int) -> 'Column':
-        """_summary_
+        """Gets a column of the matrix
 
         Args:
-            col_idx (int): _description_
+            col_idx (int): column index
 
         Returns:
-            Column: _description_
+            Column: the column
         """
 
     def iter_rows(self) -> Iterator['Row']:
-        """_summary_
+        """Iterates on the rows of the matrix
         """
 
     def iter_colums(self) -> Iterator['Column']:
-        """_summary_
+        """Iterates on the columns of the matrix
         """
 
     def scale(self, factor: float) -> Self:
-        """_summary_
+        """Scales the values of the matrix
 
         Args:
-            factor (float): _description_
+            factor (float): the scale factor
 
         Returns:
-            Self: _description_
+            Self: the matrix itself
         """
 
     def sum(self) -> float:
-        """_summary_
+        """Sums all the values of the matrix
 
         Returns:
-            float: _description_
+            float: the sum
         """
 
     def add(self, other: 'Matrix') -> None:
-        """Matrix = matrix + other
+        """Adds matrix by an other one
+        Matrix = matrix + other
 
         Args:
-            other (Matrix): _description_
+            other (Matrix): the other matrix
         """
 
     def substract(self, other: 'Matrix') -> None:
-        """Matrix = matrix - other
+        """Substracts matrix by an other one
+        Matrix = matrix - other
 
         Args:
-            other (Matrix): _description_
+            other (Matrix): the other matrix
         """
 
     def multiply(self, m1: 'Matrix', m2: 'Matrix') -> None:
-        """Matrix = M1 * M2
+        """Multiplies matrixes
+        Matrix = M1 * M2
 
         Args:
-            M1 (Matrix): _description_
-            M2 (Matrix): _description_
+            M1 (Matrix): the first matrix
+            M2 (Matrix): the second matrix
         """
 
 
@@ -177,6 +179,24 @@ class Row(Matrix):
             length (int): _description_
         """
 
+    def get(self, i: int) -> float: # type: ignore
+        """Gets a value of the row
+
+        Args:
+            i (int): index
+
+        Returns:
+            float: value
+        """
+
+    def set(self, i: int, value: float) -> None: # type: ignore
+        """Sets a value of the row
+
+        Args:
+            i (int): index
+            value (float): value
+        """
+
 
 class Column(Matrix):
     """_summary_
@@ -190,4 +210,22 @@ class Column(Matrix):
 
         Args:
             length (int): _description_
+        """
+
+    def get(self, i: int) -> float: # type: ignore
+        """Gets a value of the column
+
+        Args:
+            i (int): index
+
+        Returns:
+            float: value
+        """
+
+    def set(self, i: int, value: float) -> None: # type: ignore
+        """Sets a value of the column
+
+        Args:
+            i (int): index
+            value (float): value
         """
